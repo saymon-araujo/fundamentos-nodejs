e52d37e086a8f18f76a03a29c0b0aa8035a2f178
Saymon Araújo <saymonbrandon@gmail.com>
2022-12-31 09:37:21 -0300

    type of params

--- src/index.js
+++ src/index.js
@@ -2,15 +2,33 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+/*
+  Route Params => Servem para identificar um recurso: editar/deletar/buscar
+  Query Params => Servem para paginar / filtrar um recurso
+  Body Params => São os objetos de inserçao/alteração (JSON)
+*/
+
 app.get("/courses", (req, res) => {
+  const query = req.query;
+
+  console.log(query);
   return res.json({ cursos: ["Curso 1", "Curso 2", "Curso 3"] });
 });
 
 app.post("/courses", (req, res) => {
+  const body = req.body;
+
+  console.log(body);
+
   return res.json({ cursos: ["Curso 1", "Curso 2", "Curso 3"] });
 });
 
 app.put("/courses/:id", (req, res) => {
+  const { id } = req.params;
+
+  console.log(id);
   return res.json({ cursos: ["Curso 6", "Curso 2", "Curso 3"] });
 });
 
